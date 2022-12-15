370570c83da239a236c8b6f860dd1c4f3a132942
bbnguyenbb <bbnguyenbb@live.com>
2022-12-15 14:49:29 +0700

    finish

---
+++ CSharpNet7/SwitchForWhile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+int number1 = Random.Shared.Next(0, 9);
+int number2 = Random.Shared.Next(0, 9);
+
+Console.WriteLine($"{nameof(number1)}={number1}, {nameof(number2)}={number2}");
+
+
+Console.WriteLine("vui lòng nhập dấu của phép toán:");
+string? dau = Console.ReadLine();
+
+switch (dau)
+{
+    case "+":
+        Console.WriteLine(number1 + number2);
+        break;
+    case "-":
+        Console.WriteLine(number1 - number2);
+        break;
+    case "*":
+        Console.WriteLine(number1 + number2);
+        break;
+    case "/":
+        Console.WriteLine(number1 + number2);
+        break;
+    default:
+        Console.WriteLine("hi, i can't");
+        break;
+}
